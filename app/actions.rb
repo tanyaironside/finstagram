2c00371b0cfecbcfa5f22db06260ab97a122f89c
Tanya Ironside <tanya_ironside@hotmail.com>
2021-11-25 04:23:37 +0000

    w4d2

--- app/actions.rb
+++ app/actions.rb
@@ -5,3 +5,12 @@
     erb(:index)
     
   end
+
+  get '/signup' do    # if a user navigates to the path "signup",
+    @user = User.new  # setup empty @user object
+    erb(:signup)       # render "app/views/signup.erb"
+  end
+
+  post '/signup' do
+    params.to_s
+  end
