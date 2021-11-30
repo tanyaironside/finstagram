ce7d105bd762050fd7dcf5be4ed469fd6cec992b
Tanya Ironside <tanya_ironside@hotmail.com>
2021-11-30 04:34:10 +0000

    add get 'login', post 'login', get 'logout' routes

--- app/actions.rb
+++ app/actions.rb
@@ -1,16 +1,72 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
 
   get '/' do
-    
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
-    
   end
-
-  get '/signup' do    # if a user navigates to the path "signup",
+  
+    get '/signup' do    # if a user navigates to the path "signup",
     @user = User.new  # setup empty @user object
     erb(:signup)       # render "app/views/signup.erb"
   end
 
+  
   post '/signup' do
-    params.to_s
+    email      = params[:email]
+    avatar_url = params[:avatar_url]
+    username   = params[:username]
+    password   = params[:password]
+
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+    if @user.save
+      redirect to('/login')
+    else
+      erb(:signup)
+    end
+  end
+
+  post '/login' do
+    username = params[:username]
+    password = params[:password]
+  
+    @user = User.find_by(username: username)
+  
+    if @user && @user.password == password
+      session[:user_id] = @user.id
+      "Success! User with id #{session[:user_id]} is logged in!"
+    else
+      @error_message = "Login failed."
+      erb(:login)
+    end
+  end
+
+  get '/login' do    # when a GET request comes into /
+    erb(:login)      # render app/views/login.erb
+end
+  
+
+
+  post '/login' do    # when we submit a form with an action of /login
+    username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+  get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
   end
