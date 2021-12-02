2089a30fe458d59dba8be4e94f3df1cdab27423b
Tanya Ironside <tanya_ironside@hotmail.com>
2021-12-02 05:42:22 +0000

    adding links to make navigation user friendly and to give similar appearance to all pages

--- app/actions.rb
+++ app/actions.rb
@@ -9,7 +9,7 @@ end
     erb(:index)
   end
   
-    get '/signup' do    # if a user navigates to the path "signup",
+  get '/signup' do    # if a user navigates to the path "signup",
     @user = User.new  # setup empty @user object
     erb(:signup)       # render "app/views/signup.erb"
   end
@@ -47,26 +47,31 @@ end
 
   get '/login' do    # when a GET request comes into /
     erb(:login)      # render app/views/login.erb
-end
-  
-
-
-  post '/login' do    # when we submit a form with an action of /login
-    username = params[:username]
-  password = params[:password]
-
-  user = User.find_by(username: username)  
-
-  if user && user.password == password
-    session[:user_id] = user.id
-    redirect to('/')
-  else
-    @error_message = "Login failed."
-    erb(:login)
   end
-end
-
+  
   get '/logout' do
     session[:user_id] = nil
     redirect to('/')
+  end
+
+  get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+  end
+
+  post '/finstagram_posts' do
+    photo_url = params[:photo_url]
+
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+    if @finstagram_post.save
+      redirect(to('/'))
+    else
+      erb(:"finstagram_posts/new")
+    end
+  end
+
+  get '/finstagram_posts/:id' do
+    @finstagram_post = FinstagramPost.find(params[:id])
+    erb(:"finstagram_posts/show")
   end
--- app/models/finstagram_post.rb
+++ app/models/finstagram_post.rb
@@ -1,10 +1,14 @@
 class FinstagramPost < ActiveRecord::Base
 
+
+
     belongs_to :user
     has_many :comments
     has_many :likes
 
     validates_presence_of :user
+
+    validates :photo_url, :user, presence: true
 
     def humanized_time_ago
         time_ago_in_seconds = Time.now - self.created_at
